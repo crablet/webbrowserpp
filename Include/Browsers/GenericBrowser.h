22f2595eca2ee858b6a7714c96bb892c424e5a68
crablet <love4uandgzfc@gmail.com>
2021-05-10 12:10:19 +0800

    feat: 添加GenericBrowser通用浏览器类

---
+++ Include/Browsers/GenericBrowser.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/28.
+//
+
+#ifndef WEBBROWSER_GENERICBROWSER_H
+#define WEBBROWSER_GENERICBROWSER_H
+
+#include "BaseBrowser.h"
+
+class GenericBrowser : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_GENERICBROWSER_H
