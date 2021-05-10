66bd749bfbd8bda75da3917ba9893ef2284f64cf
crablet <love4uandgzfc@gmail.com>
2021-05-10 12:05:10 +0800

    feat: 尝试添加报错类

---
+++ Include/Error/BrowserNotFound.h
@@ -0,0 +1,15 @@
+//
+// Created by crabletchen on 2021/9/28.
+//
+
+#ifndef WEBBROWSER_BROWSERNOTFOUND_H
+#define WEBBROWSER_BROWSERNOTFOUND_H
+
+
+class BrowserNotFound
+{
+
+};
+
+
+#endif //WEBBROWSER_BROWSERNOTFOUND_H
