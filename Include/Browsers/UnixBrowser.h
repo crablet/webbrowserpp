8a9a56b6add9d61c3ffad32cf013f1242bcd6524
crablet <love4uandgzfc@gmail.com>
2021-05-11 14:13:25 +0800

    feat: 添加UnixBrowser在Unix平台的远程浏览器类

---
+++ Include/Browsers/UnixBrowser.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/28.
+//
+
+#ifndef WEBBROWSER_UNIXBROWSER_H
+#define WEBBROWSER_UNIXBROWSER_H
+
+#include "BaseBrowser.h"
+
+class UnixBrowser : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_UNIXBROWSER_H
