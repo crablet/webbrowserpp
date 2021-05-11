497218cda48d9fd2ebb217d2694d6e0a378554f5
crablet <love4uandgzfc@gmail.com>
2021-05-11 14:11:20 +0800

    feat: 添加BackgroundBrowser在后台打开的浏览器类

---
+++ Include/Browsers/BackgroundBrowser.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/28.
+//
+
+#ifndef WEBBROWSER_BACKGROUNDBROWSER_H
+#define WEBBROWSER_BACKGROUNDBROWSER_H
+
+#include "GenericBrowser.h"
+
+class BackgroundBrowser : public GenericBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_BACKGROUNDBROWSER_H
