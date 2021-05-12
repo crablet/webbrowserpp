9bd225834f5397065b75d2eef3614866c8147a96
crablet <love4uandgzfc@gmail.com>
2021-05-12 09:30:25 +0800

    feat: 添加MacOS下的火狐浏览器类

---
+++ Include/Browsers/MacOS/FirefoxMacOS.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/28.
+//
+
+#ifndef WEBBROWSER_FIREFOXMACOS_H
+#define WEBBROWSER_FIREFOXMACOS_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class FirefoxMacOS : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_FIREFOXMACOS_H
