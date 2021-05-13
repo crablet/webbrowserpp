d9c2c9099c907acb6338cdd4fd660908669c17e3
crablet <love4uandgzfc@gmail.com>
2021-05-13 10:32:25 +0800

    feat: 添加MacOS下的Safari浏览器类

---
+++ Include/Browsers/MacOS/SafariMacOS.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/28.
+//
+
+#ifndef WEBBROWSER_SAFARIMACOS_H
+#define WEBBROWSER_SAFARIMACOS_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class SafariMacOS : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_SAFARIMACOS_H
