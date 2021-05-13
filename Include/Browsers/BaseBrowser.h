1097e8c2a954325ba8f6dbe0fa73c3d35a8bb02c
crablet <love4uandgzfc@gmail.com>
2021-05-13 10:35:30 +0800

    feat: 确定浏览器接口类的接口函数

--- Include/Browsers/BaseBrowser.h
+++ Include/Browsers/BaseBrowser.h
@@ -5,10 +5,14 @@
 #ifndef WEBBROWSER_BASEBROWSER_H
 #define WEBBROWSER_BASEBROWSER_H
 
+#include <string>
 
 class BaseBrowser
 {
-
+public:
+    virtual void Open(const std::string &url, int behaviour, bool autoRaise) = 0;
+    virtual void OpenNew(const std::string &url) = 0;
+    virtual void OpenNewTab(const std::string &rl) = 0;
 };
 
 
