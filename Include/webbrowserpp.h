c38c7f0bc8fa2f651b1fe388230400862b79ad60
crablet <love4uandgzfc@gmail.com>
2021-05-14 11:40:41 +0800

    feat: 代码规范定为C++17

--- Include/webbrowserpp.h
+++ Include/webbrowserpp.h
@@ -5,6 +5,15 @@
 #ifndef WEBBROWSER_WEBBROWSERPP_H
 #define WEBBROWSER_WEBBROWSERPP_H
 
+#include <vector>
+#include <memory>
+
 #include "Browsers/BaseBrowser.h"
+
+class WebBrowserPP
+{
+private:
+    std::vector<std::unique_ptr<BaseBrowser>> tryOrder;
+};
 
 #endif //WEBBROWSER_WEBBROWSERPP_H
