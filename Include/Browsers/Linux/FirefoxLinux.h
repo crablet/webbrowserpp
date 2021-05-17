dd051e35f93092d140de9814b69f984d74bd89ab
crablet <love4uandgzfc@gmail.com>
2021-05-17 12:55:20 +0800

    feat: 添加Linux下的Firefox类

---
+++ Include/Browsers/Linux/FirefoxLinux.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_FIREFOXLINUX_H
+#define WEBBROWSER_FIREFOXLINUX_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class FirefoxLinux : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_FIREFOXLINUX_H
