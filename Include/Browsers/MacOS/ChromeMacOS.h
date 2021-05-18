485366c1eb4e8b69c6e979ce04f1ad1400256eaa
crablet <love4uandgzfc@gmail.com>
2021-05-18 13:58:30 +0800

    feat: 添加MacOS下的Chrome类

---
+++ Include/Browsers/MacOS/ChromeMacOS.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_CHROMEMACOS_H
+#define WEBBROWSER_CHROMEMACOS_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class ChromeMacOS : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_CHROMEMACOS_H
