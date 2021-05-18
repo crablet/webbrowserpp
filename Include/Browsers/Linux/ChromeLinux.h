101a5e544ba3eb793e2b65e5c3518543f9320e03
crablet <love4uandgzfc@gmail.com>
2021-05-18 13:56:30 +0800

    feat: 添加Linux下的Chrome类

---
+++ Include/Browsers/Linux/ChromeLinux.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_CHROMELINUX_H
+#define WEBBROWSER_CHROMELINUX_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class ChromeLinux : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_CHROMELINUX_H
