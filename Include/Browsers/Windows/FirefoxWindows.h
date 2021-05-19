41a1c2314684a4187eb7f1bfc4c80e37a46c31b5
crablet <love4uandgzfc@gmail.com>
2021-05-19 10:58:30 +0800

    feat: 添加Windows下的Firefox类

---
+++ Include/Browsers/Windows/FirefoxWindows.h
@@ -0,0 +1,15 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_FIREFOXWINDOWS_H
+#define WEBBROWSER_FIREFOXWINDOWS_H
+
+
+class FirefoxWindows
+{
+
+};
+
+
+#endif //WEBBROWSER_FIREFOXWINDOWS_H
