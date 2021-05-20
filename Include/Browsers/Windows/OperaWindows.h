f546af00b9c299e39c1899b305d7960ed301e372
crablet <love4uandgzfc@gmail.com>
2021-05-20 12:06:53 +0800

    feat: 添加Windows下的Opera类

---
+++ Include/Browsers/Windows/OperaWindows.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_OPERAWINDOWS_H
+#define WEBBROWSER_OPERAWINDOWS_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class OperaWindows : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_OPERAWINDOWS_H
