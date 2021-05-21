0142e7dbc5285e7d4321b33756067c7b6b70ca45
crablet <love4uandgzfc@gmail.com>
2021-05-21 13:10:53 +0800

    feat: 添加Linux下的Opera类

---
+++ Include/Browsers/Linux/OperaLinux.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_OPERALINUX_H
+#define WEBBROWSER_OPERALINUX_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class OperaLinux : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_OPERALINUX_H
