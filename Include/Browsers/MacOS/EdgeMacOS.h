d7383a13629767b653290ebde2ccc3d819e8ceb7
crablet <love4uandgzfc@gmail.com>
2021-05-22 13:14:53 +0800

    feat: 添加MacOS下的Edge类

---
+++ Include/Browsers/MacOS/EdgeMacOS.h
@@ -0,0 +1,16 @@
+//
+// Created by crabletchen on 2021/9/29.
+//
+
+#ifndef WEBBROWSER_EDGEMACOS_H
+#define WEBBROWSER_EDGEMACOS_H
+
+#include "Include/Browsers/BaseBrowser.h"
+
+class EdgeMacOS : public BaseBrowser
+{
+
+};
+
+
+#endif //WEBBROWSER_EDGEMACOS_H
