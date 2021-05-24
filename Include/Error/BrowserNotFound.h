0cf308b3c1adfd85ff52d2f14835dd6a1519f920
crablet <love4uandgzfc@gmail.com>
2021-05-24 15:21:53 +0800

    feat: 补充BrowserNotFound骨架

--- Include/Error/BrowserNotFound.h
+++ Include/Error/BrowserNotFound.h
@@ -5,10 +5,19 @@
 #ifndef WEBBROWSER_BROWSERNOTFOUND_H
 #define WEBBROWSER_BROWSERNOTFOUND_H
 
+#include <stdexcept>
+#include <string>
 
-class BrowserNotFound
+class BrowserNotFound : public std::logic_error
 {
+public:
+    explicit BrowserNotFound(const std::string &s) : std::logic_error(s) {}
+    explicit BrowserNotFound(const char *s) : std::logic_error(s) {}
 
+    virtual ~BrowserNotFound() noexcept;
+
+private:
+    std::string errorMessage;
 };
 
 
