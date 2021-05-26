37e481e5d6bceef3c1b80db9106db58212830737
crablet <love4uandgzfc@gmail.com>
2021-05-26 12:53:20 +0800

    feat: 尝试为MacOS下的火狐浏览器添加平台保护判断

--- Include/Browsers/MacOS/FirefoxMacOS.cpp
+++ Include/Browsers/MacOS/FirefoxMacOS.cpp
@@ -1,5 +1,8 @@
 //
 // Created by crablet on 2021/9/28.
 //
+#if defined(TARGET_OS_MAC) || defined(__APPLE__) || defined(__MACH__)
 
 #include "FirefoxMacOS.h"
+
+#endif
--- Include/Browsers/MacOS/FirefoxMacOS.h
+++ Include/Browsers/MacOS/FirefoxMacOS.h
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/28.
 //
+#if defined(TARGET_OS_MAC) || defined(__APPLE__) || defined(__MACH__)
 
 #ifndef WEBBROWSER_FIREFOXMACOS_H
 #define WEBBROWSER_FIREFOXMACOS_H
@@ -12,5 +13,6 @@ class FirefoxMacOS : public BaseBrowser
 
 };
 
-
 #endif //WEBBROWSER_FIREFOXMACOS_H
+
+#endif
