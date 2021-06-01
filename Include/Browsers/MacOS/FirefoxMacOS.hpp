e912d03510b053aea8485edf3894903f1a7652b3
crablet <love4uandgzfc@gmail.com>
2021-06-01 19:25:39 +0800

    feat: 规范代码写法

--- Include/Browsers/MacOS/FirefoxMacOS.hpp
+++ Include/Browsers/MacOS/FirefoxMacOS.hpp
@@ -17,7 +17,7 @@ protected:
 bool FirefoxMacOS::OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise)
 {
     const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "-a" : "-n -a";
-    std::string command = std::string("open ") + tabOrWindow + " Firefox " + url;
+    const std::string command = std::string("open ") + tabOrWindow + " Firefox " + url;
 
     std::system(command.c_str());
 
