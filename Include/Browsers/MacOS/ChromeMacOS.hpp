047432c87534643e1be99a7b5343a848231c2045
crablet <love4uandgzfc@gmail.com>
2021-06-02 20:32:49 +0800

    feat: 实现MacOS下的Chrome类

--- Include/Browsers/MacOS/ChromeMacOS.hpp
+++ Include/Browsers/MacOS/ChromeMacOS.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(TARGET_OS_MAC) || defined(__APPLE__) || defined(__MACH__)
 
 #ifndef WEBBROWSER_CHROMEMACOS_HPP
 #define WEBBROWSER_CHROMEMACOS_HPP
@@ -9,8 +10,21 @@
 
 class ChromeMacOS : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise) override;
 };
+
+bool ChromeMacOS::OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "-a" : "-n -a";
+    const std::string command = std::string("open ") + tabOrWindow + R"( "Google Chrome" )" + url;
+
+    std::system(command.c_str());
+
+    return true;
+}
 
 
 #endif //WEBBROWSER_CHROMEMACOS_HPP
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 
 #include "Include/Browsers/MacOS/FirefoxMacOS.hpp"
 #include "Include/Browsers/MacOS/SafariMacOS.hpp"
+#include "Include/Browsers/MacOS/ChromeMacOS.hpp"
 
 int main()
 {
     FirefoxMacOS().Open("https://www.tencent.com");
     SafariMacOS().Open("https://www.tencent.com");
+    ChromeMacOS().Open("https://www.tencent.com");
 
     return 0;
 }
