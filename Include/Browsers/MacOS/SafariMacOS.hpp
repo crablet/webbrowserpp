be7d0ea94aea0509c810d2822a2e4ecf6a38375d
crablet <love4uandgzfc@gmail.com>
2021-06-02 20:26:39 +0800

    feat: 实现MacOS下的Safari类

--- Include/Browsers/MacOS/SafariMacOS.hpp
+++ Include/Browsers/MacOS/SafariMacOS.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/28.
 //
+#if defined(TARGET_OS_MAC) || defined(__APPLE__) || defined(__MACH__)
 
 #ifndef WEBBROWSER_SAFARIMACOS_HPP
 #define WEBBROWSER_SAFARIMACOS_HPP
@@ -9,8 +10,21 @@
 
 class SafariMacOS : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise) override;
 };
+
+bool SafariMacOS::OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "-a" : "-n -a";
+    const std::string command = std::string("open ") + tabOrWindow + " Safari " + url;
+
+    std::system(command.c_str());
+
+    return true;
+}
 
 
 #endif //WEBBROWSER_SAFARIMACOS_HPP
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 //
 
 #include "Include/Browsers/MacOS/FirefoxMacOS.hpp"
+#include "Include/Browsers/MacOS/SafariMacOS.hpp"
 
 int main()
 {
     FirefoxMacOS().Open("https://www.tencent.com");
+    SafariMacOS().Open("https://www.tencent.com");
 
     return 0;
 }
