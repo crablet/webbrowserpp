99ce7b43caea41464c1104dc6ab950dae8aad6db
crablet <love4uandgzfc@gmail.com>
2021-06-13 12:07:53 +0800

    feat: 添加对Linux下Chrome的支持

--- Include/Browsers/Linux/ChromeLinux.hpp
+++ Include/Browsers/Linux/ChromeLinux.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(__linux__) || defined(linux) || defined(__linux)
 
 #ifndef WEBBROWSER_CHROMELINUX_HPP
 #define WEBBROWSER_CHROMELINUX_HPP
@@ -9,8 +10,19 @@
 
 class ChromeLinux : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise) override;
 };
+
+bool ChromeLinux::OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? " " : "--new-window ";
+    const std::string command = std::string("google-chrome ") + tabOrWindow + url + '&';
+
+    return std::system(command.c_str()) == 0;
+}
 
 
 #endif //WEBBROWSER_CHROMELINUX_HPP
+
+#endif
