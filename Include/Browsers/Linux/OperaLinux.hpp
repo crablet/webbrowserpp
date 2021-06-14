abd997176008448356b711255afbbfe8755f6e2b
crablet <love4uandgzfc@gmail.com>
2021-06-14 17:27:53 +0800

    feat: 添加对Linux下Opera的支持

--- Include/Browsers/Linux/OperaLinux.hpp
+++ Include/Browsers/Linux/OperaLinux.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(__linux__) || defined(linux) || defined(__linux)
 
 #ifndef WEBBROWSER_OPERALINUX_HPP
 #define WEBBROWSER_OPERALINUX_HPP
@@ -9,8 +10,19 @@
 
 class OperaLinux : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise) override;
 };
+
+bool OperaLinux::OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? " " : "--new-window ";
+    const std::string command = std::string("opera ") + tabOrWindow + url + '&';
+
+    return std::system(command.c_str()) == 0;
+}
 
 
 #endif //WEBBROWSER_OPERALINUX_HPP
+
+#endif
