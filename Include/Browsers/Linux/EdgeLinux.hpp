d6b3e5f76f8e43b2d34c19d55cbeee0d052caebe
crablet <love4uandgzfc@gmail.com>
2021-06-14 16:17:53 +0800

    feat: 添加对Linux下Edge的支持

--- Include/Browsers/Linux/EdgeLinux.hpp
+++ Include/Browsers/Linux/EdgeLinux.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(__linux__) || defined(linux) || defined(__linux)
 
 #ifndef WEBBROWSER_EDGELINUX_HPP
 #define WEBBROWSER_EDGELINUX_HPP
@@ -9,8 +10,19 @@
 
 class EdgeLinux : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise) override;
 };
+
+bool EdgeLinux::OpenImpl(const std::string &url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? " " : "--new-window ";
+    const std::string command = std::string("microsoft-edge ") + tabOrWindow + url + '&';
+
+    return std::system(command.c_str()) == 0;
+}
 
 
 #endif //WEBBROWSER_EDGELINUX_HPP
+
+#endif
