28dc413f9da9e0935caefda5f5ad970bc9cc6936
crablet <love4uandgzfc@gmail.com>
2021-06-20 19:19:21 +0800

    feat: 实现Windows下的Firefox浏览器类

--- Include/Browsers/Windows/FirefoxWindows.hpp
+++ Include/Browsers/Windows/FirefoxWindows.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
 
 #ifndef WEBBROWSER_FIREFOXWINDOWS_HPP
 #define WEBBROWSER_FIREFOXWINDOWS_HPP
@@ -9,8 +10,19 @@
 
 class FirefoxWindows : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise) override;
 };
+
+bool FirefoxWindows::OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "--new-tab " : "--new-window ";
+    const std::string command = std::string("start firefox ") + tabOrWindow + url;
+
+    return std::system(command.c_str()) == 0;
+}
 
 
 #endif //WEBBROWSER_FIREFOXWINDOWS_HPP
+
+#endif
