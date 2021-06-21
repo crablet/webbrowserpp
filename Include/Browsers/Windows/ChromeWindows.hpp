f68043986a9ed4913e350531e23f929091351be2
crablet <love4uandgzfc@gmail.com>
2021-06-21 10:11:12 +0800

    feat: 实现Windows下的Chrome浏览器类

--- Include/Browsers/Windows/ChromeWindows.hpp
+++ Include/Browsers/Windows/ChromeWindows.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
 
 #ifndef WEBBROWSER_CHROMEWINDOWS_HPP
 #define WEBBROWSER_CHROMEWINDOWS_HPP
@@ -9,8 +10,18 @@
 
 class ChromeWindows : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise) override;
 };
 
+bool ChromeWindows::OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "--new-tab " : "--new-window ";
+    const std::string command = std::string("start chrome ") + tabOrWindow + url;
+
+    return std::system(command.c_str()) == 0;
+}
 
 #endif //WEBBROWSER_CHROMEWINDOWS_HPP
+
+#endif
