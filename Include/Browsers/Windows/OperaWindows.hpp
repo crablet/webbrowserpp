d686fb820abe22580bca44ee653c20a6d2b33688
crablet <love4uandgzfc@gmail.com>
2021-06-21 13:14:20 +0800

    feat: 实现Windows下的Opera浏览器类

--- Include/Browsers/Windows/OperaWindows.hpp
+++ Include/Browsers/Windows/OperaWindows.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
 
 #ifndef WEBBROWSER_OPERAWINDOWS_HPP
 #define WEBBROWSER_OPERAWINDOWS_HPP
@@ -9,8 +10,18 @@
 
 class OperaWindows : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise) override;
 };
 
+bool OperaWindows::OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "--new-tab " : "--new-window ";
+    const std::string command = std::string("start opera ") + tabOrWindow + url;
+
+    return std::system(command.c_str()) == 0;
+}
 
 #endif //WEBBROWSER_OPERAWINDOWS_HPP
+
+#endif
