b70104104d3601613557bcf4f50679f28f5b6184
crablet <love4uandgzfc@gmail.com>
2021-06-22 19:30:53 +0800

    feat: 实现Windows下的Edge浏览器类

--- Include/Browsers/Windows/EdgeWindows.hpp
+++ Include/Browsers/Windows/EdgeWindows.hpp
@@ -1,6 +1,7 @@
 //
 // Created by crablet on 2021/9/29.
 //
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
 
 #ifndef WEBBROWSER_EDGEWINDOWS_HPP
 #define WEBBROWSER_EDGEWINDOWS_HPP
@@ -9,8 +10,18 @@
 
 class EdgeWindows : public BaseBrowser
 {
-
+protected:
+    bool OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise) override;
 };
 
+bool EdgeWindows::OpenImpl(const std::string& url, Behaviour behaviour, bool autoRaise)
+{
+    const std::string tabOrWindow = behaviour == Behaviour::NewTab ? "--new-tab " : "--new-window ";
+    const std::string command = std::string("start msedge ") + tabOrWindow + url;
+
+    return std::system(command.c_str()) == 0;
+}
 
 #endif //WEBBROWSER_EDGEWINDOWS_HPP
+
+#endif
